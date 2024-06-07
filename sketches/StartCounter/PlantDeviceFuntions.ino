8a173245fbee2e4cb6473e003663b5cc259da244
Lucas Lobo <l.lobo@campus.fct.unl.pt>
2024-06-07 12:03:09 +0100

    StartCounter fixed for joao

--- sketches/StartCounter/PlantDeviceFuntions.ino
+++ sketches/StartCounter/PlantDeviceFuntions.ino
@@ -1,51 +1,55 @@
 const char *ssid = "yourAP";
 const char *password = "yourPassword";
 
+String convertSpecialCharacters(String str) {
+  String result = "";
+  for (int i = 0; i < str.length(); i++) {
+    char c = str.charAt(i);
+    if (c == '\r') {
+      result += "\\r";
+    } else if (c == '\n') {
+      result += "\\n";
+    } else {
+      result += c;
+    }
+  }
+  return result;
+}
 
-int readHeadersGetContentLenght(WiFiClient client, bool print = false){
-  String currentLine = "";
+bool readHeaders_IsCorrectPOST(WiFiClient client, bool print = false){
   int contentLenght = 0;
+  bool firstLine = true;
 
   while (client.connected()) {
 
     if (client.available()) {            // if there's bytes to read from the client,
-      char c = client.read();            // read a byte, then
+      String line = client.readStringUntil('\n');
+      // Serial.println(line);
 
-      if(print)
-        Serial.write(c);                   // print it out the serial monitor
+      //check if it's a POST in /coordinates
+      if( firstLine && !line.startsWith("POST /coordinates")){ 
+        Serial.println("Request received but wrong one");
+        return false;
+      }
+      firstLine = false;
 
-      if (c == '\n') {                   // if the byte is a newline character
-
-        if(currentLine.startsWith("Content-Length: ")) {
-          String s = currentLine.substring(16);
-          contentLenght = s.toInt();
-        }
-
-        if (currentLine.length() == 0) {
-          return contentLenght;
-        }
-        currentLine = "";
-
-      } else if (c != '\r') {  // if you got anything else but a carriage return character,
-        currentLine += c;      // add it to the end of the currentLine
+      //line with \r means end of headers
+      if( line == "\r"){
+        Serial.println("finnished reading headers");
+        return true;
       }
     }
 
   }
-  return contentLenght;
+  return false;
 }
 
-String ReadBody(WiFiClient client, int contentLenght) {
+String ReadBody(WiFiClient client) {
   String body = "";
-  for(int i = 0; i < contentLenght; i++){
-    if(!client.connected()){ 
-      break;
-    }
 
-    char c = client.read();
-    //Serial.write(c);
-    body += c;
-  }
+  if(client.connected())
+    body = client.readString();
+  
   return body;
 }
 
@@ -69,6 +73,7 @@ Coordinates StartServer(){
 
   String latitude   = "";
   String longitude  = "";
+  bool readCoordinates = false;
 
   if (!WiFi.softAP(ssid, password)) {
     log_e("Soft AP creation failed.");
@@ -85,37 +90,38 @@ Coordinates StartServer(){
 
     if (client) {                            // if a new client connects,
       Serial.println("New Client.");         // print a message out in the serial port
-      int contentLenght = readHeadersGetContentLenght(client);
+      bool correctRequest = readHeaders_IsCorrectPOST(client);
 
+      if(correctRequest){
+        String body = ReadBody(client);
+        // Serial.println(body);
 
-      String body = ReadBody(client, contentLenght);
-      // Serial.println(body);
+        JsonDocument doc;
 
-      JsonDocument doc;
+        DeserializationError error = deserializeJson(doc, body);
 
-      DeserializationError error = deserializeJson(doc, body);
+        if (error) {
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.f_str());
+        } else {
+          String lat = doc["latitude"];
+          String longi = doc["longitude"];
 
-      if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-      } else {
-        String lat = doc["latitude"];
-        String longi = doc["longitude"];
+          latitude = lat;
+          longitude = longi;
+        }
 
-        latitude = lat;
-        longitude = longi;
+        Serial.println("Client Disconnected.");
+        Serial.println("POST data: " + body); // print the POST data
+        createRequestResponse(client);
+        break;
       }
-    
 
       createRequestResponse(client);
-
-      Serial.println("Client Disconnected.");
-      Serial.println("POST data: " + body); // print the POST data
-
-      server.close();
-      return Coordinates(latitude, longitude);
     }
   }
-  
+
+  server.close();
+  return Coordinates(latitude, longitude);
 }
 
