118b27a187893b51a372c37cbdd8b58e67747eb5
Rafael-P-G <r.graca@campus.fct.unl.pt>
2024-06-08 22:19:36 +0100

    Added fire base messaging to worker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.ui.text.google.fonts)
     implementation(libs.firebase.database)
     implementation(libs.firebase.messaging.ktx)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
