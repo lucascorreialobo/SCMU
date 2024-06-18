250c527a590069cc7e51e74ac2156e8886fce9fd
Lucas Lobo <l.lobo@campus.fct.unl.pt>
2024-06-18 08:56:21 +0100

    added actuators to worker

---
+++ sketches/worker_node/LED.ino
@@ -0,0 +1,10 @@
+#define LED_BUILTIN 17
+
+void dealLED(){
+  for(int i = 0; i < 10; i++){
+    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    delay(1000);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    delay(1000);                      // wait for a second
+  }
+}
--- sketches/worker_node/Timer_sleep.ino
+++ sketches/worker_node/Timer_sleep.ino
@@ -2,9 +2,12 @@
 #define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
 
 void start_sleep_for(int time){
+  checkActuators(20);
+
   preferences.begin("Averno", false);
   preferences.putBool("Skip Startup", true); //by default set skip startup to false, if going to forced sleep set to true
   preferences.end();
+
 
   esp_sleep_enable_timer_wakeup(time * uS_TO_S_FACTOR /*/ 4*/); // time set with variable above
   esp_deep_sleep_start();
